fedc624a2169333386f99a88e54faf78ae8edb9e
Andrea Katherine Raura Negrete <akraura@espe.edu.ec>
2024-06-02 10:08:42 -0500

    ECMS.java

--- Code/SMS/src/sms/SMS.java
+++ Code/SMS/src/sms/SMS.java
@@ -15,6 +15,7 @@ public class SMS {
      */
     public static void main(String[] args) {
         System.out.println("Proyectooooooo");
+        System.out.println("Proyecto ECMS"); 
     }
     
 }
